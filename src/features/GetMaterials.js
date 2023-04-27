98772611ed02223d034bb23496d499f911a2e4cf
Ulrik <umj4455@gmail.com>
2023-04-27 10:35:39 +0200

    GetMaterials feature prototype done.

--- src/features/GetMaterials.js
+++ src/features/GetMaterials.js
@@ -1,27 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 
 const GetMaterials = () => {
     const [data, setData] = useState(null);
     const [status, setStatus] = useState(null);
 
+    const [category, setCategory] = useState('');
     const [material, setMaterial] = useState('');
     const [property, setProperty] = useState('');
 
     const onSubmit = () => {
-        axios.get(`http://127.0.0.1:5000/api/materials/${material}/${property}`)
+        axios.get(`http://127.0.0.1:5000/api/materials/${category}/${material}/${property}`)
         .then(response => {
-            setData(response.data.data);
-            setStatus(response.data.status);
+            setData(response.data.properties);
+            setStatus({
+                code: response.status,
+                message: response.statusText
+            });
             console.log(response);
         })
         .catch(error => {
             console.error(error);
+            setStatus({
+                code: error.request.status,
+                message: error.request.statusText
+            });
+            setData(null)
         });
     }
 
   return (
-    <div className='flex flex-col w-60 bg-gray-400 p-2 rounded-lg mt-5'>
+    <div className='flex flex-col w-80 bg-gray-400 p-2 rounded-lg mt-5'>
+        <input
+            className='py-1 my-2 px-1 rounded-lg shadow-md'
+            type="text" 
+            placeholder="Category"
+            onChange={event => setCategory(event.target.value)}
+        />
         <input
             className='py-1 my-2 px-1 rounded-lg shadow-md'
             type="text" 
@@ -38,20 +53,26 @@ const GetMaterials = () => {
             className='flex justify-center py-1 my-2 bg-blue-300 rounded-lg shadow-md text-blue-600 hover:bg-blue-200' 
             onClick={onSubmit}
         >
-            Submit
+            Search
         </button>
 
         {(status) && (
-            <div className='flex flex-col space-y-2 mt-10'>
-                <div className='flex flex-row space-x-2 items-center'>
-                    <h1 className='text-green-400 bg-green-100 rounded-lg px-2 py-1'>STATUS</h1>
-                    <h1 className='font-bold text-blue-500'>{status}</h1>
+            <div className='flex flex-col space-y-2 mt-10 '>
+
+                <div className={`flex flex-row items-center pr-2 py-2 my-2 ${status.code === 200 ? 'bg-green-200' : 'bg-red-200'} rounded-lg max-w-fit`}>
+                    <p className={`font-bold ${status.code === 200 ? 'text-green-500' : 'text-red-500'} px-2 py-1 text-sm`}>{status.code}</p>
+                    <div className={`${status.code === 200 ? 'border-green-300' : 'border-red-300'} border-l-2 h-[25px] w-[1px] pr-2`}/>
+                    <h2 className={`font-bold ${status.code === 200 ? 'text-green-500' : 'text-red-500'} text-sm`}>{status.message}</h2>
                 </div>
-                <div className='flex flex-row space-x-2 items-center'>
-                    <h1 className='text-orange-400 bg-orange-100 rounded-lg px-2 py-1'>DATA</h1>
-                    {data.map((item) => (
-                        <h2 className='font-bold text-blue-500'>{item}</h2>
-                    ))}
+
+                    <div className='flex flex-col w-full pt-2'>
+                        {data?.map((item) => (
+                            <div className='flex flex-row items-center pr-2 py-2 my-0.5 bg-blue-200 rounded-lg max-w-fit cursor-pointer hover:scale-105 ease-out duration-300'>
+                                <p className='font-bold text-blue-500 px-2 py-1 text-sm'>{item.name}</p>
+                                <div className='border-blue-300 border-l-2 h-[25px] w-[1px] pr-2'/>
+                                <h2 className='font-bold text-blue-500 text-sm'>{item.value}</h2>
+                            </div>
+                        ))}
                 </div>
             </div>
         )}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,11 @@ const Home = () => {
     
 
   return (
-    <div className='flex flex-col px-10 items-center'>
-        <h1 className='font-bold text-4xl pt-10'>CNC Project</h1>
-        <GetMaterials />
+    <div className='flex pb-10'>
+        <div className='flex flex-col px-10 items-center'>
+            <h1 className='font-bold text-4xl pt-10'>CNC Project</h1>
+            <GetMaterials />
+        </div>
     </div>
   )
 }
