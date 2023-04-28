436abcdbe98dbc6a0dd8efd8efd4e6517a1abd6d
Ulrik <umj4455@gmail.com>
2023-04-28 22:28:44 +0200

    Fixed Updating Issue with material.

--- src/components/SideTabMaterialButton.js
+++ src/components/SideTabMaterialButton.js
@@ -11,7 +11,6 @@ const SideTabMaterialButton = ({
 
     const [loading, setLoading] = useState(false)
     const [materials, setMaterials] = useState([]);
-    const [isSelected, setIsSelected] = useState([]);
     const categoryRef = doc(db, 'category', category); 
 
     useEffect(() => {
@@ -41,18 +40,26 @@ const SideTabMaterialButton = ({
         }
     }, [])
 
+    const [localSelectedMaterial, setLocalSelectedMaterial] = useState(null);
+
     useEffect(() => {
-        if (!isSelected) return;
-        const thisMaterial = materials.find((material) => material.id === isSelected.id);
-        setSelectedMaterial(thisMaterial);
-    }, [materials, isSelected])
+        if (!localSelectedMaterial) return;
+        setSelectedMaterial(materials.find(material => material.id === localSelectedMaterial?.id));
+    }, [localSelectedMaterial, materials]);
+
+    useEffect(() => {
+        // if selectedMaterial not in materials, set localSelectedMaterial to null
+        if (!materials.find(material => material.id === selectedMaterial?.id)) {
+            setLocalSelectedMaterial(null);
+        }
+    }, [selectedMaterial]);
 
   return (
     <div className='flex flex-col space-y-2 px-3'>
       {materials.map((material) => (
             <button
                 key={material.id}
-                onClick={() => {setIsSelected(material);}}
+                onClick={() => {setLocalSelectedMaterial(material);}}
                 className={`text-lg bg-blue-200 text-blue-500 max-w-fit px-2 mx-2 rounded-lg 
                     cursor-pointer hover:scale-105 hover:shadow-md ease-out duration-300 mt-2
                     ${selectedMaterial?.id === material.id && 'bg-blue-500 text-white scale-105 shadow-md translate-x-3'}`}
