7a9309b2a74384f8a5b02df22208ecef3a28aa6a
Ulrik <umj4455@gmail.com>
2023-05-06 13:48:11 +0200

    Cleanup of old code.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
-import { collection, doc, getDoc, onSnapshot, query, where } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react'
-import MaterialKnown from '../features/MaterialKnown';
 
+import React from 'react'
 import SideTab from '../features/SideTab';
-import { db } from '../firebase';
 
 const Home = () => {
 
